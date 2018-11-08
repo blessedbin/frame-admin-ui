66ca57cb1a689e3f189d93e5b46945cf05e18d2f
blessedbin <xubin740723@gmail.com>
2018-11-08 17:30:25 +0800

    优化权限管理模块

--- src/views/system-manage/permission-manage/permission-manage.vue
+++ src/views/system-manage/permission-manage/permission-manage.vue
@@ -8,10 +8,10 @@
       <el-table-column label="标题" prop="title" width="150"></el-table-column>
       <el-table-column label="权限点">
         <template slot-scope="props">
-          <div style="display: inline" v-if="props.row.isLeaf && getOperations(props.row)">
-            <el-button-group v-for="tag in tags[props.row.id]" :key="tag.name" style="padding-left:5px;">
-              <el-button type="primary" plain size="mini">{{ tag.name }}</el-button>
-              <el-button type="primary" plain icon="el-icon-delete" size="mini"></el-button>
+          <div style="display: inline" v-if="props.row.isLeaf">
+            <el-button-group v-for="tag in tags[props.row.id]" :key="tag.id" style="padding-left:5px;">
+              <el-button type="primary" plain size="mini" @click="handleEditOperationApiClick(props.row)">{{ tag.name }}</el-button>
+              <el-button type="danger" plain icon="el-icon-delete" size="mini"></el-button>
             </el-button-group>
           </div>
         </template>
@@ -27,7 +27,8 @@
     <el-dialog
       title="添加" width="600px" :visible.sync="dialogAddOperation.visible">
       <div>
-        <el-form :model="dialogAddOperation.data" ref="form" label-width="80px" size="small" :rules="dialogAddOperation.rules">
+        <el-form :model="dialogAddOperation.data" ref="dialogAddOperationForm" label-width="80px" size="small"
+                 :rules="dialogAddOperation.rules">
           <el-form-item label="名称" prop="name">
             <el-input v-model="dialogAddOperation.data.name"></el-input>
           </el-form-item>
@@ -38,12 +39,55 @@
         <el-button type="primary" @click="handleAddOperationSubmit">确 定</el-button>
       </span>
     </el-dialog>
+
+    <!--编辑功能点的API-->
+    <el-dialog
+      title="添加" width="1000px" :visible.sync="dialogEditOperationApi.visible">
+      <div>
+        <div style="padding-bottom: 5px;">
+          <span>系统：</span>
+          <el-select size="small">
+            <el-option v-for="item in dialogEditOperationApi.tagOptions" :key="item.value"
+                       :label="item.label"
+                       :value="item.value">
+            </el-option>
+          </el-select>
+          <span>标签：</span>
+          <el-select size="small">
+            <el-option v-for="item in dialogEditOperationApi.tagOptions" :key="item.value"
+                       :label="item.label"
+                       :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
+        <el-table ref="multipleTable" :data="dialogEditOperationApi.tableData" tooltip-effect="dark" style="width: 100%"
+                  @selection-change="handleSelectionChange">
+          <el-table-column type="selection" width="55"></el-table-column>
+          <el-table-column label="url" prop="url"></el-table-column>
+          <el-table-column label="method" prop="method"></el-table-column>
+          <el-table-column prop="name" label="名称"></el-table-column>
+          <el-table-column prop="tags" label="标签" show-overflow-tooltip></el-table-column>
+        </el-table>
+        <div class="block" style="padding:15px;">
+          <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" style="float: right;"
+                         :current-page="dialogEditOperationApi.pagination.currentPage" :page-sizes="dialogEditOperationApi.pagination.pageSizes"
+                         :page-size="dialogEditOperationApi.pagination.pageSize" layout="total, prev, pager, next, jumper"
+                         :total="dialogEditOperationApi.pagination.total">
+          </el-pagination>
+        </div>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogEditOperationApi.visible = false">取 消</el-button>
+        <el-button type="primary" @click="handleAddOperationSubmit">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import treeTable from '@/components/TreeTable'
 import treeToArray from './customEval'
+import Vue from 'vue'
 export default {
   name: 'permission-manage',
   components: { treeTable },
@@ -51,7 +95,7 @@ export default {
     return {
       func: treeToArray,
       args: [null, null, 'timeLine'],
-      expandAll: false,
+      expandAll: true,
       treeData: {
         data: [],
         defaultProps: {
@@ -72,24 +116,36 @@ export default {
         ]
       },
       dialogAddOperation: {
+        rules: {
+          name: [
+            { required: true, message: '不能为空', trigger: 'blur' }
+          ]
+        },
         visible: false,
         data: {
           menuId: -1,
           name: ''
         }
+      },
+      dialogEditOperationApi: {
+        visible: false,
+        data: {
+          operationId: -1,
+          name: ''
+        },
+        currentRow: null,
+        tableData: [],
+        pagination: {
+          currentPage: 1,
+          pageSizes: [20, 30, 50, 100],
+          pageSize: 10,
+          total: 0
+        },
+        tagOptions: []
       }
     }
   },
   computed: {
-    // tagLoading[props.row.id] != null && tagLoading[props.row.id] === true
-    cTagLoading(id) {
-      console.log(this.tagLoading[id])
-      if (this.tagLoading[id] == null) {
-        return false
-      } else {
-        return this.tagLoading[id]
-      }
-    }
   },
   mounted() {
     this.loadMenuTreeData()
@@ -100,42 +156,30 @@ export default {
       self.treeData.loading = true
       self.$request.get('/api/ucenter/sys/menu/menu_tree.json').then(response => {
         self.treeData.data = response.data.treeList
+        self.treeData.data.forEach(value => {
+          value.tags = []
+          self.getOperations(value)
+        })
         self.treeData.loading = false
       })
-    },
-    treeNodeClick(data) {},
-    showInput() {
-      this.treeData.inputVisible = true
-      this.$nextTick(_ => {
-        this.$refs.saveTagInput.$refs.input.focus()
-      })
-    },
-    handleInputConfirm(menuId) {
-      const inputValue = this.treeData.inputValue
-      const self = this
-      if (inputValue) {
-        console.log(inputValue)
-        const data = { menuId: menuId, name: inputValue }
-        self.$request.post('/api/ucenter/sys/operation', data).then(response => {
-          self.$message.success(response.message)
-          // TODO 重新加载标签列表
-        })
-      }
-      this.treeData.inputVisible = false
-      this.treeData.inputValue = ''
     },
     getOperations(row) {
       const self = this
       const id = row.id
       const operationsId = row.operations
-      if (operationsId != null) {
+      if (row.isLeaf && operationsId != null) {
         const operationsStr = operationsId.join(',')
         self.$request.get('/api/ucenter/sys/operation?id=' + operationsStr + '&array=true').then(response => {
-          self.tags[id] = response.data
+          Vue.set(self.tags, id, response.data)
         })
       }
-      return true
+      if (row.children) {
+        row.children.forEach(value => {
+          this.getOperations(value)
+        })
+      }
     },
+    treeNodeClick(data) {},
     addOperationRow(row) {
       this.dialogAddOperation.data.menuId = row.id
       this.dialogAddOperation.data.name = ''
@@ -145,12 +189,51 @@ export default {
     // 添加功能点提交按钮
     handleAddOperationSubmit() {
       const self = this
-      self.$request.post('/api/ucenter/sys/operation', self.dialogAddOperation.data).then(response => {
-        self.$message.success(response.message)
-        self.dialogAddOperation.visible = false
-        self.getOperations(self.dialogAddOperation.row)
+      this.$refs['dialogAddOperationForm'].validate(valid => {
+        if (valid) {
+          self.$request.post('/api/ucenter/sys/operation', self.dialogAddOperation.data).then(response => {
+            self.$message.success(response.message)
+            self.dialogAddOperation.visible = false
+            self.loadMenuTreeData()
+          })
+        } else {
+          self.$message.error('参数错误')
+        }
       })
-    }
+    },
+    // /////////////////////////////////////////////
+    handleEditOperationApiClick(row) {
+      console.log(row)
+      const self = this
+      self.dialogEditOperationApi.currentRow = row
+      const url = '/api/ucenter/sys/api/datatable.json?page_num=' + self.dialogEditOperationApi.pagination.currentPage +
+        '&page_size=' + self.dialogEditOperationApi.pagination.pageSize
+      self.$request.get(url).then(response => {
+        self.dialogEditOperationApi.pagination.currentPage = response.data.currentPage
+        self.dialogEditOperationApi.pagination.pageSize = response.data.pageSize
+        self.dialogEditOperationApi.pagination.total = response.data.total
+        self.dialogEditOperationApi.pagination.pageSizes = response.data.pageSizes
+        self.dialogEditOperationApi.tableData = response.data.data
+        self.dialogEditOperationApi.visible = true
+      })
+      self.$request.get('/api/ucenter/sys/api/tag_options.json').then(response => {
+        self.dialogEditOperationApi.tagOptions = response.data
+      })
+    },
+    handleSizeChange(size) {
+      this.dialogEditOperationApi.pagination.pageSize = size
+      if (this.dialogEditOperationApi.currentRow) {
+        this.handleEditOperationApiClick(this.dialogEditOperationApi.currentRow)
+      }
+      this.loadTableData()
+    },
+    handleCurrentChange(current) {
+      this.dialogEditOperationApi.pagination.currentPage = current
+      if (this.dialogEditOperationApi.currentRow) {
+        this.handleEditOperationApiClick(this.dialogEditOperationApi.currentRow)
+      }
+    },
+    handleSelectionChange() {}
   }
 }
 </script>
